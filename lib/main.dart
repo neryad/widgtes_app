23fb7afd2d661a4e7a62776c7ec415fbcbc3974c
neryad <dayern.gomez@hotmail>
2023-03-21 17:38:04 -0400

    themeConfig

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:widgtes_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 4).getTheme(),
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
