c63d74a3da0404c7677d8771f17b7f3a4b8d3793
neryad <dayern.gomez@hotmail>
2023-03-22 17:24:53 -0400

    screens created

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgtes_app/presentation/screens/animated/aniomated_screen.dart';
+export 'package:widgtes_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgtes_app/presentation/screens/infinite_scroll/infinit_scroll_screen.dart';
+export 'package:widgtes_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgtes_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgtes_app/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:widgtes_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgtes_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgtes_app/presentation/screens/home/home_screen.dart';
---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SnackBarScreen extends StatelessWidget {
+  static const name = 'snackBar_screen';
+  const SnackBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
